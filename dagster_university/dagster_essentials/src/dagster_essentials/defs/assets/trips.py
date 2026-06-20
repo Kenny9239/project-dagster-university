49fbcc610f55c14c2ed98774d8a67f5827e39b9d
Kenny9239 <kenny9239@gmail.com>
2026-06-21 11:06:57 +1200

    sync

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -6,43 +6,62 @@ import dagster as dg
 
 from dagster_essentials.defs.partitions import monthly_partition
 
+import pandas as pd
+
 
 @dg.asset(
-    partitions_def=monthly_partition
+    partitions_def=monthly_partition,
+    group_name="raw_files",
 )
-def taxi_trips_file(context: dg.AssetExecutionContext) -> None:
-  """
+def taxi_trips_file(context) -> dg.MaterializeResult:
+    """
       The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
-  """
-
-  partition_date_str = context.partition_key
-  month_to_fetch = partition_date_str[:-3]
-
-  raw_trips = requests.get(
-      f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
-  )
-
-  with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
-      output_file.write(raw_trips.content)
-
-
-@dg.asset
-def taxi_zones_file() -> None:
     """
-      The taxi zones file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+
+    partition_date_str = context.partition_key
+    month_to_fetch = partition_date_str[:-3]
+
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
+
+    num_rows = len(pd.read_parquet(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)))
+
+    return dg.MaterializeResult(
+        metadata={
+            'Number of records': dg.MetadataValue.int(num_rows)
+        }
+    )
+
+
+@dg.asset(
+    group_name="raw_files",
+)
+def taxi_zones_file() -> dg.MaterializeResult:
     """
-    raw_zones = requests.get(
+      The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
         "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
     )
 
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
-        output_file.write(raw_zones.content)
+        output_file.write(raw_taxi_zones.content)
+    num_rows = len(pd.read_csv(constants.TAXI_ZONES_FILE_PATH))
 
-
+    return dg.MaterializeResult(
+        metadata={
+            'Number of records': dg.MetadataValue.int(num_rows)
+        }
+    )
 
 @dg.asset(
   deps=["taxi_trips_file"],
   partitions_def=monthly_partition,
+  group_name="ingested"
 )
 def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource) -> None:
   """
@@ -76,6 +95,7 @@ def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource) -> N
 # src/dagster_essentials/defs/assets/trips.py
 @dg.asset(
     deps=["taxi_zones_file"],
+    group_name="ingested"
 )
 def taxi_zones(database: DuckDBResource) -> None:
     """
